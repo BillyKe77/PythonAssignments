014c6ff37ba22f426cb48303bf51e7bb4ec7d785
BillyKe77 <blykang77@gmail.com>
2025-08-13 17:01:47 +0300

    WEEK 2 assignment

--- database/answers.sql
+++ database/answers.sql
@@ -1,4 +1,25 @@
-<!--Creating a new database-->
-CREATE DATABASE salesDB;
-<!--Switching delete the database-->
-DROP DATABASE demoDB;
+USE salesDB
+
+-- Question 2
+SELECT checkNumber, paymentDate, amount 
+FROM payments;
+-- QUESTION 3
+SELECT  orderDate, requiredDate, status 
+FROM orders
+WHERE status= "In Process"
+ORDER BY orderDate DESC;
+
+-- QUESTION 4
+SELECT firstName, lastName, email 
+FROM employees
+WHERE jobTitle= "Sales Rep"
+ORDER BY employeeNumber DESC;
+
+-- QUESTION 5
+SELECT*
+FROM offices;
+
+SELECT productName, quantityInStock 
+FROM products
+ORDER BY buyPrice ASC
+LIMIT 5;
